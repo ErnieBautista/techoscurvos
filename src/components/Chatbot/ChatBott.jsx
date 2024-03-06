35c9667c74e0c5962d7b810338f73c7edaa9d1de
Jose Luis Gomez <montalgex18@gmail.com>
2024-03-06 12:00:52 -0600

    fondo arreglado

--- src/components/Chatbot/ChatBott.jsx
+++ src/components/Chatbot/ChatBott.jsx
@@ -3,35 +3,18 @@ import ChatBot from "react-simple-chatbot";
 import emailjs from "@emailjs/browser";
 import avatar from "../../assets/imgs/Estructura metalica PBL.webp";
 import "./chatbot.scss";
-import { CustomHeader } from "../CustomHeader/customHeader";
+import { CustomHeader } from "../CustomHeader/CustomHeader";
+import { propTypes } from "react-bootstrap/esm/Image";
+import RemoveBG from "../../assets/transparente.png";
 
-
-export const ChatBott = () => {
+export const ChatBott = ({ abierto, setAbierto, handleChatBot }) => {
   const [nombre, setNombre] = useState("");
   const [telefono, setTelefono] = useState("");
   const [horario, setHorario] = useState("");
   const [consulta, setConsulta] = useState("");
   const [correo, setCorreo] = useState("");
-  const [autoOpen, setAutoOpen] = useState(false);
   const [send, setSend] = useState(false);
 
-  const [opened, setOpened] = useState(false);
-  const [delayedOpen, setDelayedOpen] = useState(false);
-
-  useEffect(() => {
-    const timeout = setTimeout(() => {
-      setDelayedOpen(true); // Después de 3 segundos, cambiar a true
-    }, 4000);
-
-    return () => clearTimeout(timeout); // Limpiar el temporizador
-  }, []); // Se ejecuta solo una vez al montar el componente
-
-  useEffect(() => {
-    if (delayedOpen) {
-      setOpened(true); // Abre el chatbot cuando delayedOpen sea true
-    }
-  }, [delayedOpen]); // Se ejecuta cuando delayedOpen cambia
-  
   useEffect(() => {
     if (send === true) {
       const templateParams = {
@@ -58,110 +41,129 @@ export const ChatBott = () => {
         );
     }
   }, [send]);
-  
+
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setAbierto(true); // Después de 3 segundos, cambiar a true
+    }, 10000);
+
+    return () => clearTimeout(timeout); // Limpiar el temporizador
+  }, []); // Se ejecuta solo una vez al montar el componente
+
   return (
-    <ChatBot
-      className="chatbot"
-      headerComponent={<CustomHeader/>}
-      hideBotAvatar
-      hideUserAvatar
-      opened={opened}
-      toggleFloating={opened}
-      botAvatar={avatar}
-      placeholder="Escribe un mensaje..."
-      floating
-      height="450px"
-      steps={[
-        {
-          id: "Saludo",
-          botDelay: "5000",
-          message:
-            "Bienvenido a Techos Curvos. Me llamo Rosy y será un placer asistirte. ¿Cuál es tu nombre?",
-          trigger: "inputNombre",
-        },
-        {
-          id: "inputNombre",
-          user: true,
-          trigger: "telefono",
-          validator: (value) => {
-            if (value) {
-              setNombre(value);
-            }
-            return true;
+    <>
+      <ChatBot
+        hideBotAvatar
+        hideUserAvatar
+        botAvatar={avatar}
+        floating
+        height="450px"
+        className="chatbot"
+        headerComponent={<CustomHeader handleChatBot={handleChatBot} />}
+        opened={abierto}
+        toggleFloating={abierto}
+        placeholder="Escribe un mensaje..."
+        steps={[
+          {
+            id: "Saludo",
+            botDelay: "5000",
+            message:
+              "Bienvenido a Techos Curvos. Me llamo Rosy y será un placer asistirte. ¿Cuál es tu nombre?",
+            trigger: "inputNombre",
           },
-        },
-        {
-          id: "telefono",
-          message:
-            "Hola {previousValue}, un gusto en conocerte, yo me encargo de ponerte en contacto con un asesor para que responda tu consulta, ¿Podrías dejarme tu teléfono completo?",
-          trigger: "inputTelefono",
-        },
-        {
-          id: "inputTelefono",
-          user: true,
-          trigger: "email",
-          validator: (value) => {
-            if (value) {
-              setTelefono(value);
-            }
-            return true;
+          {
+            id: "inputNombre",
+            user: true,
+            trigger: "telefono",
+            validator: (value) => {
+              if (value) {
+                setNombre(value);
+              }
+              return true;
+            },
           },
-        },
-        {
-          id: "email",
-          message: "¿Podrías dejarme tu correo electronico?",
-          trigger: "inputEmail",
-        },
-        {
-          id: "inputEmail",
-          user: true,
-          trigger: "horario",
-          validator: (value) => {
-            if (value) {
-              setCorreo(value);
-            }
-            return true;
+          {
+            id: "telefono",
+            message:
+              "Hola {previousValue}, un gusto en conocerte, yo me encargo de ponerte en contacto con un asesor para que responda tu consulta, ¿Podrías dejarme tu teléfono completo?",
+            trigger: "inputTelefono",
           },
-        },
-        {
-          id: "horario",
-          message: "¿En qué horario del día prefieres que te llamemos?",
-          trigger: "inputHorario",
-        },
-        {
-          id: "inputHorario",
-          user: true,
-          trigger: "consulta",
-          validator: (value) => {
-            if (value) {
-              setHorario(value);
-            }
-            return true;
+          {
+            id: "inputTelefono",
+            user: true,
+            trigger: "email",
+            validator: (value) => {
+              if (value) {
+                setTelefono(value);
+              }
+              return true;
+            },
           },
-        },
-        {
-          id: "consulta",
-          message: "¿Quieres dejarme alguna consulta adicional?",
-          trigger: "inputConsulta",
-        },
-        {
-          id: "inputConsulta",
-          user: true,
-          trigger: "despedida",
-          validator: (value) => {
-            if (value) {
-              setConsulta(value);
-              setSend(true);
-            }
-            return true;
+          {
+            id: "email",
+            message: "¿Podrías dejarme tu correo electronico?",
+            trigger: "inputEmail",
           },
-        },
-        {
-          id: "despedida",
-          message: `Con la información que me has pasado un asesor se comunicará contigo a la brevedad.`,
-          end: true,
-        },
-      ]}
-    />
+          {
+            id: "inputEmail",
+            user: true,
+            trigger: "horario",
+            validator: (value) => {
+              if (value) {
+                setCorreo(value);
+              }
+              return true;
+            },
+          },
+          {
+            id: "horario",
+            message: "¿En qué horario del día prefieres que te llamemos?",
+            trigger: "inputHorario",
+          },
+          {
+            id: "inputHorario",
+            user: true,
+            trigger: "consulta",
+            validator: (value) => {
+              if (value) {
+                setHorario(value);
+              }
+              return true;
+            },
+          },
+          {
+            id: "consulta",
+            message: "¿Quieres dejarme alguna consulta adicional?",
+            trigger: "inputConsulta",
+          },
+          {
+            id: "inputConsulta",
+            user: true,
+            trigger: "despedida",
+            validator: (value) => {
+              if (value) {
+                setConsulta(value);
+                setSend(true);
+              }
+              return true;
+            },
+          },
+          {
+            id: "despedida",
+            message: `Con la información que me has pasado un asesor se comunicará contigo a la brevedad.`,
+            end: true,
+          },
+        ]}
+      />
+      {!abierto && (
+        <img src={RemoveBG} className="iconChat" onClick={handleChatBot} />
+      )}
+    </>
   );
+};
+
+ChatBott.propTypes = {
+  handleChatBot: propTypes.func,
+  abierto: propTypes.bool,
+  setAbierto: propTypes.func,
 };
--- src/components/CustomHeader/CustomHeader.jsx
+++ src/components/CustomHeader/CustomHeader.jsx
@@ -1,21 +1,24 @@
-import React from 'react'
-import "./customHeader.css"
+import { MdClose } from "react-icons/md";
 import avatar from "../../assets/imgs/logo2.webp";
+import "./customHeader.css";
+import { propTypes } from "react-bootstrap/esm/Image";
 
-
-export const CustomHeader = () => {
+export const CustomHeader = ({ handleChatBot }) => {
   return (
-    <div className='contenedor margin-y-2'>
-      <img className='chatbotImg' src={avatar} alt="imagen de usuario" />
-      <div className='pb-0'>
-        <h3 className='usuario'>Rosy</h3>
-        <div className='onlineStatus'>
-        <span className='onlineIcon position-absolute start-10'></span> 
-        <span className='disponible'>
-        Disponible ahora
-        </span>
+    <div className="contenedor margin-y-2">
+      <img className="chatbotImg" src={avatar} alt="imagen de usuario" />
+      <div className="pb-0">
+        <h3 className="usuario">Rosy</h3>
+        <div className="onlineStatus">
+          <span className="onlineIcon position-absolute start-10"></span>
+          <span className="disponible">Disponible ahora</span>
         </div>
       </div>
+      <MdClose className="icon" onClick={handleChatBot} />
     </div>
-  )
-}
+  );
+};
+
+CustomHeader.propTypes = {
+  handleChatBot: propTypes.func,
+};
